636f2373a2f902346a7e3926f16c77f9335793ed
v_code <12273445-v_code@users.noreply.gitlab.com>
2024-06-19 08:24:34 +0300

    v0.0.6: Article page style

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,22 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
-  private apiUrl = 'https://api.spaceflightnewsapi.net/v3/articles';
+  private apiUrl = 'https://api.spaceflightnewsapi.net/v4/articles';
 
   constructor(private http: HttpClient) {}
 
   getArticles(): Observable<any[]> {
-    // console.log('Fetching articles from API');
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => response.results)
+    );
   }
 
   getArticleById(id: number): Observable<any> {
-    // console.log(`Fetching article with ID: ${id}`);
     return this.http.get<any>(`${this.apiUrl}/${id}`);
   }
 }
